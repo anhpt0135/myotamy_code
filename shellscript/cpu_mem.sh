157a73b054b7e02f6b928b7c8a3ab0a93b14a24c
anhpt <pham.anh@hubblehome.com>
2019-10-02 17:59:50 +0700

    adding tutorial for shell script

--- shellscript/cpu_mem.sh
+++ shellscript/cpu_mem.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
-echo "hello world!"
+
+echo $0 $1 $2 $3
+
+#pass argument to an array
+#${args[0] = $1}
+
+args=("$@")
+
+echo ${args[0]} ${args[1]} ${args[2]}
+echo ${args[@]}
+echo $@
+#argc:
+echo $#
+
+
