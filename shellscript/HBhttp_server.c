9d492b03c708abf08c2cfe8020d11a29d8d82f57
anhpt <pham.anh@hubblehome.com>
2019-10-04 11:04:01 +0700

    Adding ht_httpserverbk.txt to review and compare with the current file named HBhttp_server.c

--- shellscript/HBhttp_server.c
+++ shellscript/HBhttp_server.c
@@ -8,79 +8,77 @@
 	-get_disk:http://127.0.0.1:12121/?action=command=get_disk
 	and response with the required information
 */
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define SERVER_PORT 12121
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
-int main(void){
-	int ret = 0;
-	int opt = 1;
-	int sockfd, newsocket, valread;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address); 
-	char buf[]= "Doi nguoi chi song mot lan";
-	char header[] = "HTTP/1.1 200 OK\r\n"				\
-					"Connection: close\r\n"				\
-					"Content-Type: text/plain\r\n"		\
-					"Content-Length: %d\r\n\r\n";		
-	char hello[120];
+#define PORT 12121
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket; ;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    // Only this line has been changed. Everything is same.
+    char *header = "HTTP/1.1 200 OK\n"				\
+    			  "Content-Type: text/plain\n"		\
+    			  "Content-Length: %d\n\n"			\
+    			  "%s";
+    
+    char *content = "Welcome to the server page! \n\n HAVE A NICE DAY!!!!";
+    int len = strlen(content);
+   	char buf[500];
+   	sprintf(buf, header, len, content);
 
-	while(1){
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd == 0){
-		printf("cannot create a socket! \n");
-		return -1;
-	}
-	// setsockopt helps in reuse of address and port, prevent error such as: "address
-	// already in use"
-	ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-	if(ret != 0){
-		printf("error when using setsockopt\n");
-		return -1;
-	}
-
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(SERVER_PORT);
-
-	if(bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0){
-		printf("cannot bind the address with port");
-		return -1;
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    int enable = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0){
+    	printf("setsockopt(SO_REUSEADDR) failed");
+    	return -1;
 	}
 
-	if(listen(sockfd, 3) < 0){
-		printf("listen failed\n");
-		return -1;
-	}
-
-		char buffer[1024] = {0};
-		printf("Waiting new connection new connection\n");
-		newsocket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-		if(newsocket == 0){
-			printf("cannot create a new socket for the new connection\n");
-			return -1;
-		}
-
-		valread = read(newsocket, buffer, sizeof(buffer));
-		if(valread < 0){
-			printf("cannot read the buffer");
-			return -1;
-		}
-
-		printf("%s \n", buffer);
-		sprintf(hello, header,sizeof(buf));
-		printf("%s\n",hello);
-		write(newsocket, hello, sizeof(buf));
-		sprintf(hello, "%s\r\n", buf);
-		write(newsocket, buf, sizeof(buf));
-		printf("%s\n",hello);
-		close(newsocket);
-		close(sockfd);
-
-	}
-	return 0;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+   // memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , buf , strlen(buf));
+        printf("------------------Hello message sent-------------------");
+        close(new_socket);
+    }
+    return 0;
 }
