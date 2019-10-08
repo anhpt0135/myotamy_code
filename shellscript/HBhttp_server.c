382a9d021375becd209c39704e7fb97b787f011a
anhpt <pham.anh@hubblehome.com>
2019-10-08 16:13:09 +0700

    Finish http_server for mem and cpu check

--- shellscript/HBhttp_server.c
+++ shellscript/HBhttp_server.c
@@ -3,9 +3,9 @@
 	@date: Oct 3rd, 2019
 	@brief:
 	Receive the following http commands:
-	-get_memory:http://127.0.0.1:12121/?action=command=get_memory
-	-get_cpu:http://127.0.0.1:12121/?action=command=get_cpu
-	-get_disk:http://127.0.0.1:12121/?action=command=get_disk
+	-get_memory:http://127.0.0.1:12121/?action=command&command=get_memory
+	-get_cpu:http://127.0.0.1:12121/?action=command&command=get_cpu
+	-get_disk:http://127.0.0.1:12121/?action=command&command=get_disk
 	and response with the required information
 */
 #include <stdio.h>
@@ -20,6 +20,17 @@
 #define FILE_MEM_INFO "/proc/meminfo"
 #define FILE_CPU_STATE "/proc/stat"
 
+typedef struct{
+	char *name;
+	void (*func)(char *output_buf);
+}cmddb_t;
+
+void get_mem_info_func(char *output_buf);
+void get_cpu_info_func(char *output_buf);
+
+cmddb_t gcmd[]=	{{"get_memory", get_mem_info_func},
+		 		{"get_cpu", get_cpu_info_func}};
+
 void format_time(char *output){
     time_t rawtime;
     struct tm * timeinfo;
@@ -30,12 +41,34 @@ void format_time(char *output){
     sprintf(output, "[%d %d %d %d:%d:%d]",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 }
 
+void cmd_parse(char *input_buffer,char *parse_buff){
+	char *piece;
+	piece=strtok(input_buffer, "=");
+	piece=strtok(NULL, "=");
+	piece=strtok(NULL, " ");
+	strcpy(parse_buff, piece);
+	//printf("%s\n", parse_buff);
+}
+
+int vtable(char *input_buf,char *result){
+	int i;
+	int size = sizeof(gcmd)/sizeof(gcmd[0]);
+	for(i = 0; i < size; i ++){
+		if(strcmp(input_buf, gcmd[i].name) == 0){
+			if(gcmd[i].func!= NULL){
+				gcmd[i].func(result);
+				return 0;
+			}
+		}
+	}
+	return -1;
+}
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket; ;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    FILE *ptrfile;
     
     // Only this line has been changed. Everything is same.
     char *header = "HTTP/1.1 200 OK\n"				\
@@ -75,36 +108,53 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     
+    char sent_buff[1500];
+    char recv_buff[500] = {0};
+    char parse_buff[50];
+    char buftime[50];
+    char content[450];
+    int len = 0;
+
     while(1)
     {	
-    	char content[140]; //= "Welcome to the server page! \n\n HAVE A NICE DAY!!!!";
+    	//char content[1000]; //= "Welcome to the server page! \n\n HAVE A NICE DAY!!!!";
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        ptrfile = fopen(FILE_MEM_INFO, "r");
-        fread(content, sizeof(char), 140, ptrfile);
-        fclose(ptrfile);
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        
-        char buffer[500] = {0};
-        //time(&timer);
-        char buftime[200];
-        //sprintf(buftime, "%s", format_time(timer));
+
         format_time(buftime);
-        int len = strlen(content) + strlen(buftime);
-   		char buf[2000];
-   		sprintf(buf, header, len, buftime, content);
-        read( new_socket , buffer, 500);
-        //if(cmd_parse(buffer, buf) != 0){
-        //	printf("Unsupported command");
-        //	return -1;
-        //}
-        printf("%s\n",buffer );
-        write(new_socket , buf , strlen(buf));
-        printf("------------------Hello message sent-------------------");
+        read( new_socket , recv_buff, sizeof(recv_buff)/sizeof(recv_buff[0]));
+        printf("%s\n",recv_buff );
+        cmd_parse(recv_buff,parse_buff);
+        if(vtable(parse_buff,content)< 0){
+        	strcpy(content, "Unsupported Command ");
+        }
+        len = strlen(buftime) + strlen(content);
+        sprintf(sent_buff, header, len, buftime, content);
+        write(new_socket , sent_buff , strlen(sent_buff));
+        printf("------------------reply message sent-------------------");
         close(new_socket);
     }
     return 0;
+}
+
+void get_mem_info_func(char *output_buf){
+	char icontent[800];
+	FILE *ptrfile = NULL;
+	ptrfile = fopen(FILE_MEM_INFO, "r");
+    fread(icontent, sizeof(char), sizeof(icontent), ptrfile);
+    strcpy(output_buf, icontent);
+    fclose(ptrfile);
+}
+
+void get_cpu_info_func(char *output_buf){
+	char icontent[800];
+	FILE *ptrfile = NULL;
+	ptrfile = fopen(FILE_CPU_STATE, "r");
+    fread(icontent, sizeof(char), sizeof(icontent), ptrfile);
+    strcpy(output_buf, icontent);
+    fclose(ptrfile);
 }
